593d92555e2f6600d2c4cbee92754ef34e28647b
JAUSH <j05h14@yahoo.com>
2020-04-16 19:08:12 -0700

    completed conditional rendering lesson

--- Starting project/main.js
+++ Starting project/main.js
@@ -4,6 +4,8 @@ var app = new Vue({ //creates a new vue instance Vue({options})
         product: 'Socks',
         desc: 'A pair of warm, fuzzy socks',
         image: './assests/vmSocks.jpg',
-        google_link: 'https://google.com'
+        google_link: 'https://google.com',
+        inventory: 0,
+        onSale: false
     }
-})
+});
