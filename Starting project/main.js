eafad6d2c0ab0e5d71adbc1ea81db3383f369e5d
JAUSH <j05h14@yahoo.com>
2020-04-16 18:46:08 -0700

    started vue mastery course

---
+++ Starting project/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({ //creates a new vue instance Vue({options})
+    el: '#app', //putting vue instance into the div with id of 'app'
+    data: {
+        product: 'Socks',
+        desc: 'A pair of warm, fuzzy socks'
+    }
+})
